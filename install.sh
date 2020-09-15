0604397691e371a9a6a8b6fa00771adcffebef7f
Michele Romano <33063403+Mik317@users.noreply.github.com>
2020-09-15 12:30:41 +0200

    [FIX] MITM using https resource

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget http://download.redis.io/releases/redis-2.8.17.tar.gz
+wget https://download.redis.io/releases/redis-2.8.17.tar.gz
 tar xzf redis-2.8.17.tar.gz
 rm redis-2.8.17.tar.gz
 cd redis-2.8.17
